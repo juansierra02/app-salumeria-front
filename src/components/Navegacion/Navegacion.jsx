9280e5959f72dfb70317effbe3bd52835a7fab2a
juansierra02 <103540940+juansierra02@users.noreply.github.com>
2022-11-22 14:49:05 -0300

    1313

--- src/components/Navegacion/Navegacion.jsx
+++ src/components/Navegacion/Navegacion.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-//import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navegacion = () => {
     return (
@@ -16,9 +16,9 @@ const Navegacion = () => {
                     />
                 </Navbar.Brand>
                 <Nav className='me-auto'>
-                    <Nav.Link href='/'>Inicio</Nav.Link>
-                    <Nav.Link href='/CrearPedido'>Crear Pedido</Nav.Link>
-                    <Nav.Link href='/ListarPedido'>Admin</Nav.Link>
+                    <Link to='/' className='nav-link'>Inicio</Link>
+                    <Link to='/CrearPedido' className='nav-link'>Crear Pedido</Link>
+                    <Link to='/Login' className='nav-link'>Admin</Link>
                 </Nav>
             </Container>
         </Navbar>
